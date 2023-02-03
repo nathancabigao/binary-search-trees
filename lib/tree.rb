f13dc4e92e639b16e2a6a8d8c18a1f37d69dec68
nathancabigao <nathan0714@hotmail.com>
2023-02-03 11:42:18 -0500

    Add #build_tree and test with #pretty_print

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,9 +1,28 @@
 # frozen-string-literal: true
 
+require_relative 'node'
+
 # Class for creating binary search trees made of nodes.
 class Tree
   def initialize(array = nil)
     @array = array.sort.uniq
     @root = build_tree(@array)
+  end
+
+  def build_tree(array)
+    return nil if array.empty?
+
+    mid = array.size / 2
+    root = Node.new(array[mid])
+    root.left = build_tree(array.slice(0...mid))
+    root.right = build_tree(array.slice(mid + 1..array.size - 1))
+    root
+  end
+
+  # Source: https://www.theodinproject.com/lessons/ruby-binary-search-trees
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 end
---
+++ main.rb
@@ -0,0 +1,15 @@
+# frozen-string-literal: true
+
+require_relative 'lib/node'
+require_relative 'lib/tree'
+
+# Test node
+puts 'Testing, making a node with data value 9:'
+test_node = Node.new(9)
+p test_node
+
+# Test making a tree
+test_array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+puts "\nMaking a tree with array #{test_array.inspect}"
+test_tree = Tree.new(test_array)
+test_tree.pretty_print
