5fb214d60f84838f62c25200141dbd6947ca85fc
nathancabigao <nathan0714@hotmail.com>
2023-02-03 11:12:50 -0500

    Add Node class

---
+++ lib/node.rb
@@ -0,0 +1,14 @@
+# frozen-string-literal: true
+
+# Class for creating node instances to be used in Binary Search Trees.
+class Node
+  include Comparable
+
+  attr_accessor :data, :left, :right
+
+  def initialize(data = nil, left = nil, right = nil)
+    @data = data
+    @left = left
+    @right = right
+  end
+end
