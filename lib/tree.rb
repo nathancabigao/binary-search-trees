b27c8cf7f875f68e8d3ea17d157198810ff6fa87
nathancabigao <nathan0714@hotmail.com>
2023-02-03 11:19:49 -0500

    Fix #initialize for Tree

--- lib/tree.rb
+++ lib/tree.rb
@@ -3,6 +3,7 @@
 # Class for creating binary search trees made of nodes.
 class Tree
   def initialize(array = nil)
-    @array = array
+    @array = array.sort.uniq
+    @root = build_tree(@array)
   end
 end
