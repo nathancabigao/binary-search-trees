a1e3c9c3ddd99ccb917e8135852b7bc503097b5a
nathancabigao <nathan0714@hotmail.com>
2023-02-03 17:35:03 -0500

    Complete driver script

--- main.rb
+++ main.rb
@@ -15,9 +15,32 @@ test_bst = Tree.new(test_array)
 test_bst.pretty_print
 
 # Step 2. Confirm that the tree is balanced by calling #balanced?
-puts "BST Balanced? #{test_bst.balanced?}"
+puts "BST balanced? #{test_bst.balanced?}"
 
 # Step 3. Print out all elements in level, pre, post, and in order
-puts "Preorder: #{test_bst.preorder.inspect}"
+puts "\nPreorder: #{test_bst.preorder.inspect}"
 puts "Inorder: #{test_bst.inorder.inspect}"
 puts "Postorder: #{test_bst.postorder.inspect}"
+
+# Step 4. Unbalance the tree by adding several numbers > 100
+puts "\nUnbalancing the tree, adding 5 numbers over 100..."
+5.times { test_bst.insert(rand(101..200))}
+test_bst.pretty_print
+
+# Step 5. Confirm that the tree is unbalanced by calling #balanced?
+puts "BST balanced? #{test_bst.balanced?}"
+
+# Step 6. Balance the tree by calling #rebalance
+puts "\nRebalancing tree..."
+test_bst.rebalance
+test_bst.pretty_print
+
+# Step 7. Confirm that the tree is balanced by calling #balanced?
+puts "BST balanced? #{test_bst.balanced?}"
+
+# Step 8. Print out all elements in level, pre, post, and in order
+puts "\nPreorder: #{test_bst.preorder.inspect}"
+puts "Inorder: #{test_bst.inorder.inspect}"
+puts "Postorder: #{test_bst.postorder.inspect}"
+
+puts "\nTesting complete."
